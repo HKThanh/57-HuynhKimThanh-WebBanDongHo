51ebe3cbe3de4fda1f22e80b53d1b29d370ccdca
HKThanh <21086351.thanh@student.iuh.edu.vn>
2024-04-12 21:41:05 +0700

    Thêm trang ShowroomSystemPage, thiết kế lại footer

--- js/openChiTietSanPham.js
+++ js/openChiTietSanPham.js
@@ -1,6 +1,6 @@
 var cardItems = document.querySelectorAll(".card-items");
 cardItems.forEach(function(cardItem) {
     cardItem.addEventListener("click", function() {
-        window.location.href = "./Chitietsanpham.html";
+        window.location.href = "./ProductDetailPage.html";
     })
 })
